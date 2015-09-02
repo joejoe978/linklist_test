4948a8e124dea74d56e4377cc2420290d7d1e069
joejoe978 <r03922133@ntu.edu.tw>
2015-09-02 23:22:06 +0800

    9/2

--- LeetCode_remove_linklist.c
+++ LeetCode_remove_linklist.c
@@ -5,21 +5,33 @@
 struct ListNode {
     int val;
     struct ListNode *next;
-}*head;
+}*First;
 
-void deleteNode(struct ListNode* node) {
-    node->val = node->next->val;
-    node->next = node->next->next;
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    if (head==NULL) return;
+    if (head->val==val && head->next==NULL){
+        head = NULL;
+        return ;
+    }
+    while(head->next != NULL){
+        if (head->val == val){
+            head->val = head->next->val;
+            head->next = head->next->next;
+        }
+        else
+            head = head->next ;
+    }
+
 }
 
 int main(){
-    head = NULL;
-
     struct ListNode *tmp1 =(struct ListNode*)malloc(sizeof(struct ListNode));
     struct ListNode *tmp2 =(struct ListNode*)malloc(sizeof(struct ListNode));
     struct ListNode *tmp3 =(struct ListNode*)malloc(sizeof(struct ListNode));
     struct ListNode *tmp4 =(struct ListNode*)malloc(sizeof(struct ListNode));
-    head = tmp1;
+    struct ListNode *tmp2a =(struct ListNode*)malloc(sizeof(struct ListNode));
+
+    First = tmp1;
     tmp1->val = 1;
     tmp1->next = tmp2;
     tmp2->val = 2;
@@ -27,16 +39,18 @@ int main(){
     tmp3->val = 3;
     tmp3->next = tmp4;
     tmp4->val = 4;
-    tmp4->next = NULL;
+    tmp4->next = tmp2a;
+    tmp2a->val = 2;
+    tmp2a->next = NULL;
 
     print();
-    deleteNode(tmp3);
+    removeElements(head, 2);
     print();
     return 0;
 }
 
 void print(){
-    struct ListNode *now = head;
+    struct ListNode *now = First;
     while(now->next!=NULL){
         printf("%d -> ",now->val);
         now = now->next;
