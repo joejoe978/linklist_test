0fb2cabadddd2ab868187848cbdb2a51890cafcd
joejoe978 <r03922133@ntu.edu.tw>
2015-09-15 10:58:56 +0800

    do not need size variable

--- LeetCode_Stack_by_linklist.c
+++ LeetCode_Stack_by_linklist.c
@@ -8,8 +8,6 @@ struct ListNode {
     int val;
     struct ListNode *next;
 }*top;
-int size = 0;
-
 
 void print(){
     struct ListNode* now = top;
@@ -21,35 +19,29 @@ void print(){
     //free(now);
 }
 
+bool empty(){
+    return top==NULL;
+}
+
 void push(int num){
     struct ListNode* node = (struct ListNode*)malloc(sizeof(int));
     node->val = num;
     node->next = top;
     top = node;
-    size++;
 }
 
 int pop(){
-    if(size == 0) return INT_MIN;
+    if(empty()) return INT_MIN;
     struct ListNode* tmp = top;
     int value;
     value = tmp->val;
     top = tmp->next;
     free(tmp);
-    size--;
     return value;
 }
 
 struct ListNode* findtop(){
     return top;
-}
-
-int findsize(){
-    return size;
-}
-
-bool empty(){
-    return size==0;
 }
 
 int main(){
@@ -58,8 +50,10 @@ int main(){
     print();
     printf("now pop:%d \n",pop());
     print();
-    printf("now size:%d \n",findsize());
     printf("now top:%d \n",findtop()->val);
+    printf("now pop:%d \n",pop());
     printf(empty() ? "true" : "false");
+    printf(" \n");
+    printf("now pop:%d \n",pop());
     return 0;
 }
