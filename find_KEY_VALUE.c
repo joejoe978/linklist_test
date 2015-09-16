139681b1d7f055c434a42e79945794c5bb306039
joejoe978 <r03922133@ntu.edu.tw>
2015-09-17 00:10:45 +0800

    new question

---
+++ find_KEY_VALUE.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <string.h>
+
+string get(string key);
+
+int main(){
+
+    char A[100] = "key=10・\0・key1=20・\0・";
+
+    printf("A: %c \n",A[1]);
+
+    return 0;
+}
+
+string get(string key){
+
+
+}
